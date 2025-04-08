ddb6add0f87188889ab0f47a0d3abb0cbd4cc786
Mohamed <traoremohamed7212@gmail.com>
2025-04-08 12:33:34 +0000

    il reste la mise en page A4

--- script.js
+++ script.js
@@ -12,28 +12,55 @@ function changerCouleur(){
     const base = document.getElementById('base');
     base.style.backgroundColor = "#7751b3"
 }
-function telecharger() {
-    const element = document.getElementById("contenue"); //Le contenu qu'on voudrait telecharger
+
+/*function telecharger() {
+    const element = document.getElementById("contenue"); //Le contenu qu'on
   
     const options = {
         margin: [0, 0, 0, 0],
-        filename: 'document.pdf',
+        filename: 'monCV.pdf',
         image: { type: 'jpeg', quality: 1 },
         html2canvas: { 
             scrollX: 438,
             scrollY: 0,
             scale: 2,
-            windowWidth: element.scrollWidth, // Prend toute la largeur du contenu
-            windowHeight: element.scrollHeight // prend la hauteur
+            windowWidth:  element.scrollWidth, // Prend toute la largeur du contenu
+            windowHeight: element.scrollHeight // S'assure que le rendu respecte la largeur
         },
         jsPDF: { 
             unit: 'mm', 
-            format: 'a4', // format a4
+            format: [element.scrollWidth * 0.264583, element.scrollHeight * 0.264583], // Convertit px en mm
             orientation: element.scrollWidth > element.scrollHeight ? 'landscape' : 'portrait'
         }
     };
 
     html2pdf().set(options).from(element).save();
+} */
+function telecharger() 
+{
+        let element = document.getElementById('contenue');
+        let opt = {
+            margin: 0, // Marges réduites au minimum
+            filename: 'cv.pdf',
+            image: { type: 'jpeg', quality: 1 }, // Qualité max
+            html2canvas: { 
+                scale: 2, // Augmenté pour meilleure qualité
+                scrollX: 250,
+                scrollY: 0,
+                windowWidth: element.scrollWidth,
+                windowHeight: element.scrollHeight,
+                useCORS: true, //pour la securité concernant les liens externes
+            },
+            jsPDF: { 
+                unit: 'mm', 
+                format: 'a4', 
+                orientation: 'portrait',
+               // hotfixes: ["px_scaling"] // Correction des pixels
+            }
+    
+        };
+        html2pdf().set(opt).from(element).save();
 }
+
   
  
